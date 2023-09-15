884f7b2ca0ac6b9edb2b78f05334b02128cae0d1
orbitlens <orbit.lens@gmail.com>
2023-09-15 20:04:12 +0300

    Fix scheduled update available range

--- views/server-config/action-form-layout.js
+++ views/server-config/action-form-layout.js
@@ -3,22 +3,32 @@ import {observer} from 'mobx-react'
 import {runInAction} from 'mobx'
 import {DateSelector} from '@stellar-expert/ui-framework'
 
+function trimSeconds(date) {
+    if (typeof date === 'number') {
+        date = new Date(date)
+    }
+    return date.toISOString().replace(/:\d{2}\.\d*Z/, '')
+}
+
 export default observer(function ActionFormLayout({settings, children}) {
+    const {timestamp} = settings
+    const min = trimSeconds(new Date().getTime() + 30 * 60 * 1000)
+    const max = trimSeconds(new Date().getTime() + 10 * 24 * 60 * 60 * 1000)
     const updateTimestamp = useCallback(val => {
         runInAction(() => settings.data.timestamp = val * 1000) //timestamp in milliseconds
         settings.validate()
     }, [settings])
 
-    return <div className='row'>
+    return <div className="row">
         <div className="column column-66">
             <div className="space"/>
             {children}
         </div>
         <div className="column column-33">
             <div className="space"/>
-            <b>Scheduled quorum update (UTC)</b>
-            <DateSelector className="micro-space" style={{width: '100%'}}
-                          value={settings.data.timestamp || ''} onChange={updateTimestamp}/>
+            <label>Scheduled quorum update time (UTC)
+                <DateSelector className="micro-space" style={{width: '100%'}} value={timestamp || ''} onChange={updateTimestamp} min={min} max={max}/>
+            </label>
         </div>
     </div>
 })
