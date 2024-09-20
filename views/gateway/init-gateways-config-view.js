b4c6fdecba1180944e41e2911ae405b79f5dec2f
orbitlens <orbit.lens@gmail.com>
2024-09-20 09:46:42 +0000

    Add download link for gateway config

--- views/gateway/init-gateways-config-view.js
+++ views/gateway/init-gateways-config-view.js
@@ -6,7 +6,6 @@ export default function InitGatewaysConfigView({forceSetup = false}) {
     const [setup, setSetup] = useState(forceSetup)
     const [gateways, setGateways] = useState([])
     const [challenge, setChallenge] = useState(() => generateRandomChallenge())
-    const gatewayConfig = JSON.stringify({urls: gateways, challenge}, null, 2)
 
     if (!setup)
         return <p className="space">
@@ -14,11 +13,15 @@ export default function InitGatewaysConfigView({forceSetup = false}) {
             If there's no gateway configuration yet, <a href="#" onClick={() => setSetup(true)}>create new initial config</a>.
         </p>
 
+    const gatewayConfig = JSON.stringify({urls: gateways, challenge}, null, 2)
+    const downloadHref = 'data:application/json,' + encodeURIComponent(gatewayConfig)
+
     return <div>
         <h3>Initial gateways configuration</h3>
         <GatewayListView gateways={gateways} challenge={challenge} updateGateways={setGateways} alwaysReveal/>
         {gateways.length >= 3 && <div className="space">
-            <CopyToClipboard text={gatewayConfig}><a href="#" className="icon-copy">Copy generated config</a></CopyToClipboard> and save it
+            <a href={downloadHref} className="icon-download-circle" download="gateways.json">Download</a> or{' '}
+            <CopyToClipboard text={gatewayConfig}><a href="#" className="icon-copy">copy</a></CopyToClipboard> generated config and save it
             as <code>gateways.json</code> in the node home directory.
         </div>}
     </div>
